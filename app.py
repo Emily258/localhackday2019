04233ccbc019c08a1fb90f9770dc31cdb7c7d013
Emily Wong <Emily258@users.noreply.github.com>
2019-11-30 18:28:28 -0800

    Built front end template

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/diet', methods=['GET'])
 def diet():
     vegetarian = request.args.get('Vegetarian')
-    
+    vegan = request.args.get('Vegan')
+    celiac = request.args.get('Celiac')
+
     if (vegetarian is not ''):
         return render_template('vegetarian.html')
 
